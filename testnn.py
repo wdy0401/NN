7949675bea0ce07bf1d1b913dbc4c67445b33382
wdy0401 <wdy0401@gmail.com>
2018-07-03 16:24:35 +0800

    神经网络学习开始

--- testnn.py
+++ testnn.py
@@ -8,7 +8,7 @@ import mnist_loader
 training_data, validation_data, test_data = \
     mnist_loader.load_data_wrapper()
 import network
-net = network.Network([784, 30, 10])
+net = network.Network([784, 50, 10])
 
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net.SGD(training_data, 3, 10, 3.0, test_data=test_data)
 
